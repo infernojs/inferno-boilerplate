f13a1fd84074eb09a211ae36dc93e93cf0267156
Dominic Gannaway <ADM@Dominics-MacBook-Pro.local>
2016-10-29 11:18:25 +0100

    updated boilerplate version

---
+++ README.md
@@ -0,0 +1,11 @@
+# inferno-boilerplate
+
+## Overview
+
+This is a very basic boilerplate example for people wanting to get started with Inferno. This boilerplate uses Inferno 1.0.0-beta5, Webpack, Babel and the Inferno JSX Babel plugin.
+
+## Getting started
+
+Clone this repository locally and then run `npm install`.
+
+To run the example app, run `npm start`. View `http://localhost:8080` in your browser to see the boilerplate example.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import Inferno from 'inferno';
-import InfernoDOM from 'inferno-dom';
+import Component from 'inferno-component';
+
+class MyApp extends Component {
+	constructor(props) {
+		super(props);
+		this.showVersion = this.showVersion.bind(this);
+	}
+	showVersion() {
+		alert(`The version is: ${ this.props.version }!`);
+	}
+	render({ version }) {
+		return (
+			<div>
+				<h1>Inferno Boilerplate</h1>
+				<p>This is an Inferno Boilerplate example using <em>Inferno { version }</em>.</p>
+				<button onClick={ this.showVersion }>Show version</button>
+			</div>
+		);
+	}
+}
+
+Inferno.render(<MyApp version='1.0.0-beta5' />, document.getElementById('app'));
 
