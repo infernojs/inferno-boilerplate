2eed6df30c6b4f95bfa71f3771acc0c1fe3c72f1
Roman Sokolov <roman.sokolov.hq@leroymerlin.ru>
2019-06-21 14:21:15 +0300

    Update index.js for new version of inferno.

--- src/index.js
+++ src/index.js
@@ -1,60 +1,66 @@
 // inferno module
-import {render} from 'inferno';
+import { render } from 'inferno';
 
 // routing modules
-import { Router, Link, Route, IndexRoute } from 'inferno-router';
-import createBrowserHistory from 'history/createBrowserHistory';
+import { BrowserRouter, Switch, Link, Route } from 'inferno-router';
+import { createBrowserHistory } from 'history';
+import MyApp from './MyApp';
 
 const browserHistory = createBrowserHistory();
 function App({ children }) {
-    return (
-		<div>
-			<h1>Application</h1>
-			<Link to="/users">User list</Link>
-			<br />
-			<Link to="/users/user/tester">Tester's page</Link>
-            {children}
-		</div>
-    )
+  return (
+    <div>
+      <h1>Application</h1>
+      <Link to="/users">User list</Link>
+      <br />
+      <Link to="/users/user/tester">Tester's page</Link>
+      <br />
+      <Link to="/my-app">My App</Link>
+      {children}
+    </div>
+  );
 }
 
 function NoMatch({ children, params }) {
-    return (
-		<div>no match</div>
-    )
+  return <div>no match</div>;
 }
 
 function Home({ children }) {
-    return (
-		<div>home</div>
-    )
+  return <div>home</div>;
 }
 
 // `children` in this case will be the `User` component
-function Users({ children, params }) {
-
-    return (
-		<div>
-			<h2>user list</h2>
-            {children}
-		</div>
-    )
+function Users({ match }) {
+  return (
+    <div>
+      <h2>user list</h2>
+      <Route path={match.url + '/user/:username'} component={User} />
+    </div>
+  );
 }
 
-function User({ params }) {
-    return <h1>{JSON.stringify(params)}</h1>
+function User({ match }) {
+  return <h1>{JSON.stringify(match.params)}</h1>;
 }
 
 const routes = (
-	<Router history={browserHistory}>
-		<Route component={App}>
-			<IndexRoute component={Home} />
-			<Route path="/users" component={Users}>
-				<Route path="/user/:username" component={User} />
-			</Route>
-			<Route path="*" component={NoMatch} />
-		</Route>
-	</Router>
+  <BrowserRouter history={browserHistory}>
+    <div>
+      <h1>Application</h1>
+      <Link to="/users">User list</Link>
+      <br />
+      <Link to="/users/user/tester">Tester's page</Link>
+      <br />
+      <Link to="/my-app">My App</Link>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/users" component={Users} />
+        <Route path="/my-app" component={MyApp} />
+        <Route path="*" component={NoMatch} />
+      </Switch>
+    </div>
+  </BrowserRouter>
 );
 
 render(routes, document.getElementById('app'));
